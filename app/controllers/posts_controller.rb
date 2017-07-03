45c8bc7bef936a0f0f7c7aed5fdfbe9591a90421
Corey Gibson <cgibsonmm@gmail.com>
2017-07-03 19:55:37 -0400

    added new and create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
   def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = 'Post successfully created!'
+      redirect_to posts_path
+    else
+      flash[:danger] = 'Post unsuccessfully created!'
+      render 'new'
+    end
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end 
 end
